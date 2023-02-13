26907f89974a61bfdb8132fe9cad4f5bc68b8e4b
smaton1 <sam.maton91@gmail.com>
2023-02-13 22:35:03 +0000

    cli
    - Added a language selection,
    - Started on file creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use colored::Colorize;
+use dialoguer::{console::Term, theme::ColorfulTheme, Select};
+use std::io::{self, Write};
+
+struct UserSelections {
+    project_name: String,
+    laguage: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut user_selections = UserSelections {
+        project_name: String::new(),
+        laguage: String::new(),
+    };
+
+    let items = vec!["Javascript".yellow(), "Typescript".blue()];
+
+    //Finish File creation
+
+    // let mut file = std::fs::File::create("data.txt").expect("create failed");
+
+    // file.write_all("Hello World".as_bytes())
+    //     .expect("write failed");
+    // file.write_all("\nTutorialsPoint".as_bytes())
+    //     .expect("write failed");
+    // println!("data written to file");
+
+    println!("Enter project name:");
+
+    io::stdin()
+        .read_line(&mut user_selections.project_name)
+        .expect("Enter a valid name");
+
+    println!("Select language:");
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .items(&items)
+        .default(0)
+        .interact_on_opt(&Term::stderr())
+        .expect("Select an item");
+
+    match selection {
+        Some(index) => user_selections.laguage = items[index].to_string(),
+        None => println!("User did not select anything"),
+    }
+
+    println!(
+        "{}, {}",
+        user_selections.project_name, user_selections.laguage
+    );
 }
