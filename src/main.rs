0f37aff8836a7940a9e30082dca197aa17f925b1
smaton1 <sam.maton91@gmail.com>
2023-02-18 09:20:26 +0000

    Added some stuff for file creation

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,18 @@
 use colored::Colorize;
 use dialoguer::{console::Term, theme::ColorfulTheme, Select};
+use npm_package_json::Package;
+use serde::ser::{Serialize, SerializeStruct, Serializer};
 use std::io::{self, Write};
 
-struct UserSelections {
-    project_name: String,
-    laguage: String,
-}
-
 fn main() {
-    let mut user_selections = UserSelections {
-        project_name: String::new(),
-        laguage: String::new(),
-    };
+    let mut new_package = Package::new();
 
     let items = vec!["Javascript".yellow(), "Typescript".blue()];
-
-    //Finish File creation
-
-    // let mut file = std::fs::File::create("data.txt").expect("create failed");
-
-    // file.write_all("Hello World".as_bytes())
-    //     .expect("write failed");
-    // file.write_all("\nTutorialsPoint".as_bytes())
-    //     .expect("write failed");
-    // println!("data written to file");
 
     println!("Enter project name:");
 
     io::stdin()
-        .read_line(&mut user_selections.project_name)
+        .read_line(&mut new_package.name)
         .expect("Enter a valid name");
 
     println!("Select language:");
@@ -39,13 +23,16 @@ fn main() {
         .interact_on_opt(&Term::stderr())
         .expect("Select an item");
 
-    match selection {
-        Some(index) => user_selections.laguage = items[index].to_string(),
-        None => println!("User did not select anything"),
-    }
+    // match selection {
+    //     Some(index) => user_selections.laguage = items[index].to_string(),
+    //     None => println!("User did not select anything"),
+    // }
 
-    println!(
-        "{}, {}",
-        user_selections.project_name, user_selections.laguage
-    );
+    //Finish File creation
+
+    let mut file = std::fs::File::create("package.json").expect("create failed");
+
+    file.write_all(new_package.as_bytes())
+        .expect("write failed");
+    println!("data written to file");
 }
